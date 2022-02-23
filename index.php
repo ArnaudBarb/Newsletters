7935102c8089fc5f36afcf55692cdc6fe472e48c
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-23 10:06:11 +0100

    insertion require et autoLoadFunction()

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once './functions/autoLoadFunction.php';
+session_start();
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_ALL, 'fr_FR', 'fr', 'FR', 'fr_FR@euro');
+
+spl_autoload_register(function ($className) {
+    include './classes/' . $className . '.php';
+});
