558bad327c5d914e6de8423eed45d86da3e748de
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-24 09:21:48 +0100

    formulaire pour renseigner des thèmes

---
+++ includes/frmThemes.php
@@ -0,0 +1,12 @@
+<form action="index.php?page=themes" method="post">
+    <ul>
+        <li>
+            <label for="subjectContent">Thème :</label>
+        <input type="text" id="subjectcontent" name="subjectcontent" value="<?php echo $subjectContent;?>" />
+    </li>
+        <li>
+            <input type="reset" value="Effacer" />
+            <input type="submit" value="Valider le choix" name="validation" />
+        </li>
+    </ul>
+</form>
