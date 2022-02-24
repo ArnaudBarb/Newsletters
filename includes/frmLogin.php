e288aab5eefb252c26661acd5513aa032d45ce3f
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-24 09:21:18 +0100

    formulaire de login (html parts)

--- includes/frmLogin.php
+++ includes/frmLogin.php
@@ -2,7 +2,7 @@
     <label for="mail">e-mail</label>
     <input type="text" id="mail" name="mail">
     <label for="mdp">Mot de passe</label>
-    <input type="text" id="mdp" name="mdp">
+    <input type="password" id="mdp" name="mdp">
     <input type="reset" value="Effacer">
     <input type="submit" value="Envoyer" name="envoi">
 </form>
