fbd75036b48e4b74a87f954554e7e6eabe684f18
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-24 16:41:20 +0100

    ras

--- includes/frmThemes.php
+++ includes/frmThemes.php
@@ -2,11 +2,12 @@
     <ul>
         <li>
             <label for="subjectContent">Thème :</label>
-        <input type="text" id="subjectcontent" name="subjectcontent" value="<?php echo $subjectContent;?>" />
-    </li>
+            <input type="text" id="subjectcontent" name="subjectcontent" value="<?php echo $subjectContent;?>" />
+        </li>        
         <li>
             <input type="reset" value="Effacer" />
             <input type="submit" value="Valider le choix" name="validation" />
         </li>
     </ul>
+
 </form>
